926c9b41f54284805ba0994c6d7b34f4006f3f09
Miguel Garcia <miguelgarse@gmail.com>
2021-05-16 11:36:46 +0200

    Library ModelMapper added to map from DTOs to entities and viceversa

--- src/main/java/es/upm/etsisi/iot/IotApplication.java
+++ src/main/java/es/upm/etsisi/iot/IotApplication.java
@@ -1,7 +1,9 @@
 package es.upm.etsisi.iot;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IotApplication {
@@ -10,4 +12,9 @@ public class IotApplication {
 		SpringApplication.run(IotApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+	    return new ModelMapper();
+	}
+	
 }
